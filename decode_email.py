9f9e461daa579d3b38e9cea4e1688bb089383b8e
Guillaume Gauvrit <guillaume@gandi.net>
2017-02-01 18:15:02 +0100

    Print headers

--- decode_email.py
+++ decode_email.py
@@ -6,5 +6,9 @@ import sys
 
 msg = email.message_from_string(sys.stdin.read())
 
+for key, val in msg.items():
+    print("{}: {}".format(key, val))
+
+print()
 print(msg.get_payload(decode=True).decode('utf-8'))
 
